1f30f8002ae1eb51f6095831350a20f66b03bd6a
Jennifer Schlierman <jd9913@gmail.com>
2020-11-28 15:25:09 -0700

    correcting spelling errors

--- server.js
+++ server.js
@@ -4,21 +4,21 @@ const mongoose=require('mongoose');
 
 const app=express();
 
-const PORT =process.env.PORT||3300;
+const PORT =process.env.PORT||33031;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-networking',{
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/social-networking',{
     useFindAndModify: false,
-    useNewUrlParser: true,
+    useNewUrlParser: false,
     useUnifiedTopology:true
 
 });
 
 mongoose.set('debug', true);
 
-app.listenerCount(PORT, ()=> console.log(`🌍 Connected on localhost:${PORT}`));
+app.listen(PORT, ()=> console.log(`Connected on localhost:${PORT}`));
 
