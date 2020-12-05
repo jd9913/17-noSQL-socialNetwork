e8989ce6b02a67804bc900ce3d5490e4488390ab
Jennifer Schlierman <jd9913@gmail.com>
2020-12-05 12:42:07 -0700

    update routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -16,27 +16,27 @@ const {
     router
     .route('/')
     .get(getAllThoughts)  //working
-    .post(addThought);
+    .post(addThought); //working
     
 //GET/PUT/DELETE thought by ID API: /api/thoughts/:id
 router
 .route('/:id')
 .get(getThoughtById)  //working
 .put(updateThought)  //not working
-.delete(removeThought)
+.delete(removeThought) // not working
 
 //API: /api/thoughts/:thoughtId/reactions
 
 router
 .route("/:thoughtId/reactions")
-.post(addReaction)
+.post(addReaction) //not working
 
 
 // API: //api/thoughts/:thoughtId/reactions/:reactionId
 
 router
 .route('/:thoughtId/reactions/:reactionId')
-.delete(removeReaction);
+.delete(removeReaction); //not working
 
 
 
